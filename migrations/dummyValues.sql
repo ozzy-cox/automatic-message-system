070cc75a5c8c60e734e342cb772b0230a30e1c7c
ozank <ozan.koksal@turboard.com>
2024-12-18 09:27:10 +0300

    Fix: dummy data creation script

--- migrations/dummyValues.sql
+++ migrations/dummyValues.sql
@@ -3,8 +3,5 @@ SELECT
     'Message content ' || gs.id AS content,
     LPAD((1000000000 + FLOOR(random() * 8999999999))::TEXT, 10, '0') AS to_,
     FALSE AS sending_status,
-    CASE 
-        WHEN random() > 0.5 THEN NOW() - (random() * INTERVAL '30 days') 
-        ELSE NULL 
-    END AS sent_at
+    NULL AS sent_at
 FROM generate_series(1, 1000) AS gs(id);
